aed90a09e9929a618fedbc0c629dc347dc9e45a3
YairWeisman <YairWeisman@gmail.com>
2017-04-28 00:54:33 +0300

    added card

--- we_train/src/App.js
+++ we_train/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar/SearchBar.js';
-import Header from './components/header/Header.js';
+import Header from './components/Header/Header.js';
+import Card from './components/Card/Card.js';
 
 class App extends Component {
 
@@ -10,6 +11,7 @@ class App extends Component {
     	<div className="container-fluid">
 	    	<Header/>
 			<SearchBar/>
+			<Card/>
     	</div>
     );
   }
