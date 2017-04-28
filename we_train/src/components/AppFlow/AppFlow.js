4c1f9e863874ccb5058fe4bb03220c3f6be8261f
YairWeisman <YairWeisman@gmail.com>
2017-04-28 08:06:35 +0300

    bodyy

--- we_train/src/components/AppFlow/AppFlow.js
+++ we_train/src/components/AppFlow/AppFlow.js
@@ -11,14 +11,7 @@ class AppFlow extends Component {
 					<Col lgOffset={2} lg={8}>HOW DOES IT WORK?</Col>
 				</Row>
 				<Row className="flow-pics">
-					<Col lgOffset={1} lg={3} className="stage1"></Col>
-					<Col lg={3} className="stage2"></Col>
-					<Col lg={3} className="stage3"></Col>
-				</Row>
-				<Row className="flow-text">
-					<Col lgOffset={1} lg={3} className="stage1-desc"></Col>
-					<Col lg={3} className="stage2-desc"></Col>
-					<Col lg={3} className="stage3-desc"></Col>
+					<Col lgOffset={1} lg={10} className="stage1"></Col>
 				</Row>
 			</div>
 		);
