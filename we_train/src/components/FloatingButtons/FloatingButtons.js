adeda6cd00e7a6b92dfd54ad8981b647465da50c
YairWeisman <YairWeisman@gmail.com>
2017-04-28 10:35:34 +0300

    fixing some styling

--- we_train/src/components/FloatingButtons/FloatingButtons.js
+++ we_train/src/components/FloatingButtons/FloatingButtons.js
@@ -5,11 +5,11 @@ class FloatingButtons extends Component {
 
   render() {
     return (
-    	<div>
-          <span><i className="material-icons md-48 star">stars</i></span>
+    	<div className="floatings">
+          <span><i className="floating material-icons md-48 star">stars</i></span>
           <div className="circle" onClick={() => window.scrollTo(0, 0)}>
-            <span><i className="material-icons md-48 up1">expand_less</i></span>
-            <span><i className="material-icons md-48 up2">expand_less</i></span>
+            <span><i className="floating material-icons md-48 up1">expand_less</i></span>
+            <span><i className="floating material-icons md-48 up2">expand_less</i></span>
           </div>
     	</div>
     );
